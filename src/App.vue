72551749a8f57c1752ef8f5957faf729c3ac3373
dawsoncanby <dawsoncanby@gmail.com>
2020-04-22 16:09:14 -0600

    waves

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content class="base-container">
+      <Home/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.base-container {
+  background: linear-gradient(#09f, #003280);
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container fluid class="fill-height pa-0 base-container">
+    <img :src="wave" class="background-image">
+    <v-row class="z-1">
+      <v-col class="text-center">
+        <v-card
+          max-width="300"
+          class="mx-auto">
+          <v-card-text class="pa-1">
+            <h1>Supa Squidds</h1>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import wave from '../assets/wave.svg'
+export default {
+  data() {
+    return {
+      wave,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.background-image {
+  position: absolute;
+  top: 0;
+  z-index: 0;
+  animation: move-wave 2s linear infinite;
+}
+
+.z-1 {
+  position: relative;
+  z-index: 1;
+}
+
+@keyframes move-wave {
+  0% { top: 0px }
+  50% { top: -5px }
+  100% { top: 0px }
+}
+</style>
